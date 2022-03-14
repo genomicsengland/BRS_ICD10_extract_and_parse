696a9dc49962c05ca25936af4339534878feb15c
Chris Odhams <chris.odhams@genomicsengland.co.uk>
2022-03-14 20:10:47 +0000

    Update icd10_extract_and_parse.R

--- icd10_extract_and_parse.R
+++ icd10_extract_and_parse.R
@@ -16,6 +16,10 @@ fetch_lk <- function(sql) {
     mutate(across(everything(), as.character))
 }
 
+# ICD10 Code Look-up ------------------------------------------------------
+coding <- read_tsv("coding19.tsv", col_names = T) %>% 
+  select(icd10 = coding, icd10_description = meaning)
+
 # Fetch ICD10 entries from all tables -------------------------------------
 icd10_full <- bind_rows(
   
@@ -63,11 +67,25 @@ icd10_clean <- icd10_full %>%
 icd10_unique <- icd10_clean %>% 
   distinct(participant_id, icd10)
 
+# ICD10 code count and description ----------------------------------------
+icd10_count <- icd10_unique %>% 
+  count(icd10, sort = TRUE, name = "n_participants") %>% 
+  left_join(coding, by = "icd10") %>%
+  mutate(icd10_description = case_when(
+    icd10 == "R69X6" ~ "Null (Primary diagnosis)",
+    icd10 == "R69X8" ~ "Invalid",
+    icd10 == "R69X3" ~ "Invalid (Exter l Cause code entered as Primary Diagnosis)",
+    is.na(icd10_description) ~ "No description found", 
+    TRUE ~ icd10_description)) %>% 
+  select(icd10, icd10_description, n_participants)
+
 # Save outputs ------------------------------------------------------------
 save(icd10_full, file = "icd10_full.RData")
 save(icd10_clean, file = "icd10_clean.RData")
 save(icd10_unique, file = "icd10_unique.RData")
+save(icd10_count, file = "icd10_count.RData")
 
 write_tsv(icd10_full, file = "icd10_full.tsv", col_names = TRUE)
 write_tsv(icd10_clean, file = "icd10_clean.tsv", col_names = TRUE)
 write_tsv(icd10_unique, file = "icd10_unique.tsv", col_names = TRUE)
+write_tsv(icd10_count, file = "icd10_count.tsv", col_names = TRUE)
